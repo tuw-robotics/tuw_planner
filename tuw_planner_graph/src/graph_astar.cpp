50adcf1b0d0fc03586df32d9baf4a67accd69168
Markus Bader <markus.bader@tuwien.ac.at>
2024-01-11 10:48:25 +0100

    straight line working

--- tuw_planner_graph/src/graph_astar.cpp
+++ tuw_planner_graph/src/graph_astar.cpp
@@ -1,54 +1,14 @@
-/*********************************************************************
- *
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2020 Shivang Patel
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * Author: Shivang Patel
- *
- * Reference tutorial:
- * https://navigation.ros.org/tutorials/docs/writing_new_nav2planner_plugin.html
- *********************************************************************/
-
 #include <cmath>
 #include <string>
 #include <memory>
 #include "nav2_util/node_utils.hpp"
 
-#include "tuw_planner_straight/graph_planner.hpp"
+#include "tuw_planner_graph/graph_astar.hpp"
 
-namespace tuw_planner_straight
+namespace tuw_planner_graph
 {
 
-void GraphPlanner::configure(
+void GraphAStar::configure(
   const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
   std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
   std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
@@ -66,28 +26,28 @@ void GraphPlanner::configure(
   node_->get_parameter(name_ + ".interpolation_resolution", interpolation_resolution_);
 }
 
-void GraphPlanner::cleanup()
+void GraphAStar::cleanup()
 {
   RCLCPP_INFO(
     node_->get_logger(), "CleaningUp plugin %s of type NavfnPlanner",
     name_.c_str());
 }
 
-void GraphPlanner::activate()
+void GraphAStar::activate()
 {
   RCLCPP_INFO(
     node_->get_logger(), "Activating plugin %s of type NavfnPlanner",
     name_.c_str());
 }
 
-void GraphPlanner::deactivate()
+void GraphAStar::deactivate()
 {
   RCLCPP_INFO(
     node_->get_logger(), "Deactivating plugin %s of type NavfnPlanner",
     name_.c_str());
 }
 
-nav_msgs::msg::Path GraphPlanner::createPlan(
+nav_msgs::msg::Path GraphAStar::createPlan(
   const geometry_msgs::msg::PoseStamped & start,
   const geometry_msgs::msg::PoseStamped & goal)
 {
@@ -141,7 +101,7 @@ nav_msgs::msg::Path GraphPlanner::createPlan(
   return global_path;
 }
 
-}  // namespace tuw_planner_straight
+}  // namespace tuw_planner_graph
 
 #include "pluginlib/class_list_macros.hpp"
-PLUGINLIB_EXPORT_CLASS(tuw_planner_straight::GraphPlanner, nav2_core::GlobalPlanner)
+PLUGINLIB_EXPORT_CLASS(tuw_planner_graph::GraphAStar, nav2_core::GlobalPlanner)
--- tuw_planner_straight/README.md
+++ tuw_planner_straight/README.md
@@ -5,14 +5,7 @@ This planner is a copy from the [tutorial](https://navigation.ros.org/plugin_tut
 
 ```bash
 ros2 launch tuw_nav2 nav2_minimal_launch.py planner_server_yaml:=empty # run nav2 without planner_server
-ros2 run nav2_planner planner_server --ros-args --params-file $WS/src/tuw_planner/tuw_planner_straight/config/nav2/planner_tuw_graph.yaml 
+ros2 run nav2_planner planner_server --ros-args --params-file $WS/src/tuw_planner/tuw_planner_straight/config/nav2/straight_line.yaml 
 ```
 ## graph_planner
 This planner plans a path based on a predifined graph
-### usage
-
-```bash
-ros2 launch tuw_nav2 nav2_minimal_launch.py planner_server_yaml:=empty # run nav2 without planner_server
-ros2 run nav2_planner planner_server --ros-args --params-file $WS/src/tuw_planner/tuw_planner_straight/config/nav2/planner_tuw_graph.yaml 
-
-```
